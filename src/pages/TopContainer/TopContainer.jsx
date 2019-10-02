457df59dba040691a4c5e18c89b062d3ec3773a3
junkisai <jonki.beam@gmail.com>
2019-10-02 17:36:18 +0900

    New: scroll比較

--- src/pages/TopContainer/TopContainer.jsx
+++ src/pages/TopContainer/TopContainer.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import List from '../../components/List';
 import VirtualList from '../../components/VirtualList';
+
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
 
 class TopContainer extends React.Component {
   constructor(props) {
@@ -8,15 +16,54 @@ class TopContainer extends React.Component {
   }
 
   render() {
-    const list = [...Array(500).keys()].map(i => i + 1700);
+    let years = [...Array(500).keys()].map(i => i + 1700);
+    let months = [...Array(12).keys()].map(i => i + 1);
+    let days = [...Array(30).keys()].map(i => i + 1);
 
     return(
-      <VirtualList
-        list={list}
-        viewNum={5}
-        itemNum={list.length}
-        itemHeight={30}
-      />
+      <div>
+        <FlexBox>
+          <VirtualList
+            list={years}
+            viewNum={5}
+            itemNum={years.length}
+            itemHeight={30}
+          />
+          <VirtualList
+            list={months}
+            viewNum={5}
+            itemNum={months.length}
+            itemHeight={30}
+          />
+          <VirtualList
+            list={days}
+            viewNum={5}
+            itemNum={days.length}
+            itemHeight={30}
+          />
+        </FlexBox>
+
+        <FlexBox>
+          <List
+            list={years}
+            viewNum={5}
+            itemNum={years.length}
+            itemHeight={30}
+          />
+          <List
+            list={months}
+            viewNum={5}
+            itemNum={months.length}
+            itemHeight={30}
+          />
+          <List
+            list={days}
+            viewNum={5}
+            itemNum={days.length}
+            itemHeight={30}
+          />
+        </FlexBox>
+      </div>
     );
   }
 }
